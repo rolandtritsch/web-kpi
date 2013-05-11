aded9274e126ba8bb37992ccbc96dc32db37f0e3
Roland Tritsch <rtritsch@gilt.com>
2013-05-11 18:24:27 +0100

    Add all the heroku stuff

--- project/Build.scala
+++ project/Build.scala
@@ -2,17 +2,20 @@ package org.tritsch.scala.webkpi.build
 
 import sbt._
 import sbt.Keys._
+
 import play._
 
-object WebKpiBuild extends sbt.Build {
+import com.typesafe.sbt.SbtStartScript
 
-  val appName = "web-kpi-app"
+object WebKpiBuild extends sbt.Build {
+  val appName = "web-kpi"
   val appVersion = "1.0"
 
   val appDependencies = Seq(
     "org.scalaj" % "scalaj-time_2.9.1" % "0.6"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies)
-
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    SbtStartScript.startScriptForClassesSettings: _*
+  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,4 +2,8 @@
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 // Use the Play sbt plugin for Play projects
-addSbtPlugin("play" % "sbt-plugin" % "2.1.0")
+addSbtPlugin("play" % "sbt-plugin" % "2.1.1")
+
+// Add the start script plugin (needed by heroku)
+addSbtPlugin("com.typesafe.sbt" % "sbt-start-script" % "0.7.0")
+//addSbtPlugin("com.typesafe.startscript" % "xsbt-start-script-plugin" % "0.5.2")
